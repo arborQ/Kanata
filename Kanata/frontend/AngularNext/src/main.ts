e8b5e998b7ddc0fe1d47072686b30a08af169e9e
Lukasz Wojcik <lukasz.wojcik@smtsoftware.com>
2017-01-11 13:26:53 +0100

    its alive

--- Kanata/frontend/AngularNext/src/main.ts
+++ Kanata/frontend/AngularNext/src/main.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule } from '@angular/platform-browser';
 
-// @Component({
-//   selector: 'my-app',
-//   template: '<div>test app</div>'
-// })
-export class AppComponent { }
+@Component({
+  selector: 'my-app',
+  template: '<div>test app {{AppName}}</div>'
+})
+class AppComponent {
+    AppName= 'test';
+}
+
+@NgModule({
+  bootstrap: [ AppComponent ],
+  imports: [ BrowserModule ],
+  declarations: [
+    AppComponent 
+  ]
+})
+class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
--- Kanata/frontend/AngularNext/webpack.config.js
+++ Kanata/frontend/AngularNext/webpack.config.js
@@ -1,33 +1,35 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = function (options) {
     return {
         entry: {
             app: './src/main.ts',
-            vendor: './src/vendors.ts'
+            vendor: './src/vendors.ts',
+            polyfills: './src/polyfills.ts'
         },
 
         output: {
-            filename: '[name].js'
+            filename: './dest/[name].js?_=[hash]',
+            chunkFilename: './dest/[id].chunk.js?_=[hash]'
         },
         resolve: {
             extensions: ['', '.ts', '.js']
         },
-        loaders: [
-            {
-                test: /\.ts$/,
-                loaders: ['ts', 'awesome-typescript-loader', 'angular2-template-loader']
-            },
-            {
-                test: /\.css$/,
-                loaders: 'style!css'
-            }
-        ],
+        devtool: 'source-map',
+        module: {
+            loaders: [
+                {
+                    test: /\.ts$/,
+                    loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+                }
+            ]
+        },
         plugins: [
-            // new webpack.optimize.UglifyJsPlugin(),
             new webpack.optimize.CommonsChunkPlugin({
                 name: ['app', 'vendor', 'polyfills']
             }),
+            new webpack.optimize.UglifyJsPlugin(),
             new HtmlWebpackPlugin({
                 template: 'src/index.html'
             })
